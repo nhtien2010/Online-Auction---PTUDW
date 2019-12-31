5b86274012660269ff8eda70ea3cbd9f3d13ae67
ErodNelps <40132636+ErodNelps@users.noreply.github.com>
2019-12-31 10:24:16 +0700

    Revert "Thuong"

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 let express = require('express');
 let app = express();
+//Set Public Static folder
 
 app.use(express.static(__dirname + '/public'));
 
+//View Engine
 let expressHbs = require('express-handlebars');
 let hbs = expressHbs.create({
     extname: 'hbs',
     defaultLayout: 'layout',
-    helpers: require("./helpers/handlebars.js").helpers,
     layoutsDir: __dirname + '/views/layouts/',
     partialsDir: __dirname + '/views/partials/'
 });
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 
-require('./middlewares/routemiddleware')(app);
+//Define routes
+app.get('/', (req, res) =>{
+    res.render('index');
+});
 
+//Server Port
 app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), () => {
-    console.log(`Server is runnning at port ${app.get('port')}`)
+    console.log(`Server is running at port ${app.get('port')}`);
 });
