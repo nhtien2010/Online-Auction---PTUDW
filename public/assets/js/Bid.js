62879dbb36e763cd49028875006f9a6e17a7ed3f
ErodNelps <ptmtdoraemon@gmail.com>
2019-12-31 10:23:15 +0700

    Merge edited files

--- public/assets/js/Bid.js
+++ public/assets/js/Bid.js
@@ -1,24 +1,24 @@
 function Bid() {
     if(!isNaN(document.getElementById("offer").value) && document.getElementById("offer").value != "") {
-        var current = parseFloat(document.getElementById("productinformation").getElementsByTagName("b")[1].innerHTML);
+        var current = parseFloat(document.getElementById("current").innerHTML);
         var offer = parseFloat(document.getElementById("offer").value);
-        var deal = parseFloat(document.getElementById("productinformation").getElementsByTagName("p")[2].innerHTML.split(" ")[1]);
-        var time = parseInt(document.getElementById("productinformation").getElementsByTagName("b")[0].innerHTML.split(" ")[1]);
-
+        var cap = parseFloat(document.getElementById("cap").innerHTML.split(" ")[1]);
+        var bids = parseInt(document.getElementById("bids").innerHTML.split(" ")[1]);
+        
         if(offer <= current)
             alert("Offer is too low!");
-        else if(offer >= deal) {
-            document.getElementById("productinformation").getElementsByTagName("b")[1].innerHTML = deal + '$';
-            document.getElementById("productinformation").getElementsByTagName("p")[3].innerHTML = 'Bidder: Erod Nelps (9.2 Rated)';
-            document.getElementById("productinformation").getElementsByTagName("h2")[0].innerHTML += ' (Sold)';
-            document.getElementById("productinformation").getElementsByTagName("b")[0].innerHTML = 'Bidded: ' + (time + 1) + ' times';
+        else if(offer >= cap) {
+            document.getElementById("current").innerHTML = cap + '$';
+            document.getElementById("holder").innerHTML = 'Bidder: Erod Nelps (9.2 Rated)';
+            document.getElementById("name").innerHTML += ' (Sold)';
+            document.getElementById("bids").innerHTML = 'Bids: ' + (bids + 1) + ' Times';
             document.getElementById("offer").disabled = true;
             alert("Bought!");
         }
         else {
-            document.getElementById("productinformation").getElementsByTagName("b")[1].innerHTML = offer + '$';
-            document.getElementById("productinformation").getElementsByTagName("p")[3].innerHTML = 'Bidder: Erod Nelps (9.2 Rated)';
-            document.getElementById("productinformation").getElementsByTagName("b")[0].innerHTML = 'Bidded: ' + (time + 1) + ' times';
+            document.getElementById("current").innerHTML = offer + '$';
+            document.getElementById("holder").innerHTML = 'Bidder: Erod Nelps (9.2 Rated)';
+            document.getElementById("bids").innerHTML = 'Bids: ' + (bids + 1) + ' Times';
             alert("Complete!");
         }
     }
