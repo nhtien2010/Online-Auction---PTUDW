606ac7cc53e627cfc4d8184e534ff03dccf0f046
ErodNelps <ptmtdoraemon@gmail.com>
2019-12-31 10:33:09 +0700

    Merge updated files

--- Updated files/models/categorymodel.js
+++
@@ -1,6 +0,0 @@
-const db = require('../utils/db');
-
-module.exports = {
-    all: _ => (db.select('select * from category')),
-    id: id => db.select(`select * from category where category.id = ${id}`)
-};
--- public/assets/js/Sort.js
+++ public/assets/js/Sort.js
@@ -1,47 +1,45 @@
+var offset = document.getElementById("offset").innerHTML;
+var order = document.getElementById("order");
+Sort();
 function Sort() {
+    var list = document.getElementsByClassName("product");
+    list = Array.prototype.slice.call(list);
     var sortingorder = order.value;
-    var i, switching, b, shouldSwitch, first, second, text;
-    switching = true;
-    while (switching) {
-      switching = false;
-      b = list[0].getElementsByTagName("ul")[0].getElementsByTagName("li");
-      for (i = 0; i < (b.length - 1); i++) {
-        shouldSwitch = false;
-        if(sortingorder === "price") {
-            first = parseInt(b[i].getElementsByClassName(sortingorder)[0].innerHTML);
-            second = parseInt(b[i + 1].getElementsByClassName(sortingorder)[0].innerHTML);
-            if ( first > second) {
-                shouldSwitch = true;
-                break;
-            }
+    var first, second, text;
+    list.sort(function (a, b) {
+        if (sortingorder == 5) {
+            first = parseInt(a.children[0].getElementsByTagName("p")[sortingorder].innerHTML);
+            second = parseInt(b.children[0].getElementsByTagName("p")[sortingorder].innerHTML);
+            return first - second;
         }
         else {
-            text = b[i].getElementsByClassName(sortingorder)[0].innerHTML.toLowerCase();
-            if(text.indexOf('d') != -1)
-                first = parseInt(text) * 1440;
-            else if(text.indexOf('h') != -1)
+            text = a.children[0].getElementsByTagName("p")[sortingorder].innerHTML.toLowerCase();
+            if (text.indexOf('d') != -1)
+                first = parseInt(text) * 86400;
+            else if (text.indexOf('h') != -1)
+                first = parseInt(text) * 3600;
+            else if (text.indexOf('m') != -1)
                 first = parseInt(text) * 60;
             else
                 first = parseInt(text);
-            
-            text = b[i + 1].getElementsByClassName(sortingorder)[0].innerHTML.toLowerCase();
-            if(text.indexOf('d') != -1)
-                second = parseInt(text) * 1440;
-            else if(text.indexOf('h') != -1)
+
+            text = b.children[0].getElementsByTagName("p")[sortingorder].innerHTML.toLowerCase();
+            if (text.indexOf('d') != -1)
+                second = parseInt(text) * 86400;
+            else if (text.indexOf('h') != -1)
+                second = parseInt(text) * 3600;
+            else if (text.indexOf('m') != -1)
                 second = parseInt(text) * 60;
             else
                 second = parseInt(text);
-                
-            if (first < second) {
-                shouldSwitch = true;
-                break;
-            }
-        }
-      }
-      if (shouldSwitch) {
-            b[i].parentNode.insertBefore(b[i + 1], b[i]);
 
-        switching = true;
-      }
+            return second - first;
+        }
+    });
+
+    $('.page').empty();
+
+    for (var i = 0, len = list.length; i < len; i++) {
+        $('.page')[parseInt(i / offset)].appendChild(list[i]);
     }
 }
