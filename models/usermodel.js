d4288ada7f32f201331f16d9164f9f988f48e8bd
ErodNelps <ptmtdoraemon@gmail.com>
2020-01-06 10:01:21 +0700

    update user profile hbs and DB

--- models/usermodel.js
+++ models/usermodel.js
@@ -4,5 +4,12 @@ module.exports = {
     id: id => db.select(`select * from user where id=${id}`),
     add: entity => db.insert(entity, 'user'),
     check: email => db.select(`select * from user where email='${email}'`),
-    update: (entity, condition) => db.update(entity, condition, 'user')
+    update: (entity, condition) => db.update(entity, condition, 'user'),
+    singleByUserName: async username => {
+        const rows = await db.load(`select * from user where username = '${username}'`);
+        if (rows.length > 0)
+          return rows[0];
+    
+        return null;
+      }
 };
