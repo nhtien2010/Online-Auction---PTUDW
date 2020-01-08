52501cc7df4993de9216f48776b465c8b4dc022d
Tien <34505433+nhtLeNa@users.noreply.github.com>
2020-01-08 16:57:27 +0700

    Add files via upload

--- models/productmodel.js
+++ models/productmodel.js
@@ -8,9 +8,15 @@ module.exports = {
     search: text => db.select(`select distinct * from product where match(name, description) against ('${text}' in natural language mode)`),
     detail: id => db.select(`select * from product where product.id = ${id}`),
     related: category => db.select(`select * from product where product.category = ${category} order by product.start desc limit 5`),
+    refresh: _ => db.refresh(),
     countByCat: async category => {
         const row = await db.select( `select * from product where product.category = ${category}`);
         return row.length;
     },
-    refresh: _ => db.refresh()
+    all: _ => (db.select('select * from product')),
+    delete: prodid => db.delete({id: prodid}, 'product'),
+    countByUser: async userid => {
+        const row = await db.select(`select * from product where product.seller = ${userid}`);
+        return row.length;
+    }
 };
