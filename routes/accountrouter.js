ebc4ce77e182341bddef5159fa207f19617e75bb
ErodNelps <ptmtdoraemon@gmail.com>
2020-01-10 08:06:08 +0700

    fix profile

--- routes/accountrouter.js
+++ routes/accountrouter.js
@@ -19,7 +19,7 @@ router.get('/profile', async function (req, res) {
         name: req.session.user.name,
         email: req.session.user.email,
         dob: req.session.user.dob,
-        priviledge: req.session.user.priviledge,
+        privilege: req.session.user.privilege,
         address: req.session.user.address,
     })
 });
@@ -63,4 +63,115 @@ router.post('/upgrade', async function (req, res) {
     res.redirect('#');
 });
 
+router.post('/profile/:name/edit-name', async function (req, res, next) {
+    usermodel.singleByUsername(req.body.newusername)
+        .then(user => {
+            if (user) {
+                alert("Username exist!");
+                return res.redirect('/user/profile/' + req.session.user.name);
+            } else {
+                usermodel
+                .update({
+                    name: req.body.newusername
+                }, {
+                    where: { name: req.session.user.name }
+                })
+                .then(function() {
+                    usermodel
+                    .singleByUsername(req.body.newusername)
+                    .then(user => {
+                        req.session.user = user;
+                        res.locals.user = req.session.user;
+                        res.redirect('/');
+                    })
+                    .catch(error => next(error));
+                })
+                .catch(function(error) {
+                    res.json(error);
+                    console.log("update profile failed!");
+                });
+            }
+        });
+    res.render('./profile', {
+        user: req.session.user,
+        name: req.session.user.name,
+        email: req.session.user.email,
+        dob: req.session.user.dob,
+        privilege: req.session.user.privilege,
+        address: req.session.user.address,
+    });
+});
+
+router.post('/profile/:name/edit-email', async function (req, res, next) {
+    usermodel.singleByEmail(req.body.newemail)
+        .then(user => {
+            if (user) {
+                alert("Email exist!");
+                return res.redirect('/user/profile/' + req.session.user.name);
+            } else {
+                usermodel
+                .update({
+                    email: req.body.newemail
+                }, {
+                    where: { name: req.session.user.email }
+                })
+                .then(function() {
+                    usermodel
+                    .singleByEmail(req.body.newemail)
+                    .then(user => {
+                        req.session.user = user;
+                        res.locals.user = req.session.user;
+                        res.redirect('/');
+                    })
+                    .catch(error => next(error));
+                })
+                .catch(function(error) {
+                    res.json(error);
+                    console.log("update profile failed!");
+                });
+            }
+        });
+    res.render('./profile', {
+        user: req.session.user,
+        name: req.session.user.name,
+        email: req.session.user.email,
+        dob: req.session.user.dob,
+        privilege: req.session.user.privilege,
+        address: req.session.user.address,
+    })  
+});
+
+router.post('/profile/:name/edit-dob', async function (req, res, next) {
+    usermodel.singleByUsername(req.params.name)
+        .then(function() {
+                usermodel
+                .update({
+                    dob: req.body.newdob
+                }, {
+                    where: { name: req.session.user.mame }
+                })
+                .then(function() {
+                    usermodel
+                    .singleByUsername(req.params.name)
+                    .then(user => {
+                        req.session.user = user;
+                        res.locals.user = req.session.user;
+                        res.redirect('/');
+                    })
+                    .catch(error => next(error));
+                })
+                .catch(function(error) {
+                    res.json(error);
+                    console.log("update profile failed!");
+                });
+        });
+    res.render('./profile', {
+        user: req.session.user,
+        name: req.session.user.name,
+        email: req.session.user.email,
+        dob: req.session.user.dob,
+        privilege: req.session.user.privilege,
+        address: req.session.user.address,
+    });
+});
 module.exports = router;
